21755caaed8d74f98bc2dd1945dd5293895300a3
lookOnTheBrightSide <nkosindlovu87@gmail.com>
2015-07-29 23:00:46 +0200

    more client side stuff

--- leadb.js
+++ leadb.js
@@ -5,25 +5,77 @@ Posts = new Mongo.Collection('posts');
 if (Meteor.isClient) {
     // counter starts at 0
     console.log('#### NEVER PASTE ANY CODE IN HERE!')
-    console.log(PlayersList.find({sort:{score: -1}}).fetch());
+    //console.log(PlayersList.findOne({sort:{score: -1}}));
 
     Template.leaderBoard.helpers({
         players: function() {
-            return PlayersList.find().fetch();
-        }
-    });
-    Template.index.helpers({
-        playersCount: function() {
-            return PlayersList.find().count();
+            return PlayersList.find({},{sort:{score: -1, name: 1}});
         },
-        topPlayer: function() {
-            return PlayersList.find({sort:{'score': 1},limit:1}).fetch();
-            
-        },
-        lowPlayer: function() {
-            return PlayersList.find({sort:{'score': -1},limit:1}).fetch();
-        }
+      'selectedClass': function(){
+        var playerId = this._id;
+        var selectedPlayer = Session.get('selectedPlayer');
+        if (playerId === selectedPlayer) {
+          return 'playerSelectedClass'
+        };
+      }
     });
+    Template.leaderBoard.events({
+      'click .playerWell': function () {
+        //session name then value
+        var selectedOne = this._id;
+        Session.set('selectedPlayer', selectedOne);
+        var selectedOne = Session.get('selectedPlayer');
+        //console.log(selectedOne);
+        //document.getElementsByClassName('playerWell').IdName = selectedOne;
+      },
+      'focus .playerSelected': function(){
+        console.log('all focus on me now!');
+      },
+      'change .playerSelected': function(){
+        console.log('tupac changes is playing');
+      },
+      'click .points': function(){
+        var selectedPlayer = Session.get('selectedPlayer');
+        PlayersList.update(selectedPlayer, {$inc:{score: 5}});
+        console.log(selectedPlayer);
+      },
+      'click .pointsMinus': function(){
+        var selectedPlayer = Session.get('selectedPlayer');
+        PlayersList.update(selectedPlayer, {$inc:{score: -5}});
+      },
+      'click .remove': function(){
+        var selectedPlayer = Session.get('selectedPlayer');
+        if(confirm('Remove this player?')){PlayersList.remove(selectedPlayer)};
+      }
+    });
+    Template.addPlayerForm.events({
+      'submit form': function (event) {
+        event.preventDefault();
+        var playerName = event.target.name.value;
+        var age = event.target.age.value;
+        PlayersList.insert({
+          name: playerName,
+          score: 0,
+          age: age
+        })
+        var playerName = "";
+        var age = "";
+        console.log('submitted form');
+        console.log(playerName, age);
+      }
+    });
+  Template.index.helpers({
+      playersCount: function() {
+          return PlayersList.find().count();
+      },
+      topPlayer: function() {
+          return PlayersList.find({}, {sort:{'score': -1},limit:1});
+              
+      },
+      lowPlayer: function() {
+          return PlayersList.find({}, {sort:{'score': 1},limit:1});
+      }
+  });
 }
 
 if (Meteor.isServer) {
@@ -33,3 +85,49 @@ if (Meteor.isServer) {
 
     });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Template.index.helpers({
+//     playersCount: function() {
+//         return PlayersList.find().fetch().count();
+//     },
+//     topPlayer: function() {
+//         return PlayersList.find({}, {sort:{'score': 1},limit:1}).fetch();
+            
+//     },
+//     lowPlayer: function() {
+//         return PlayersList.find({}, {sort:{'score': -1},limit:1}).fetch();
+//     }
+// });
